8bd0d3de04ca124747f28762c834c58c14b1cfcd
Bas Nijholt <basnijholt@gmail.com>
2018-10-01 14:03:59 +0200

    add 'AverageLearner1D'

--- adaptive/__init__.py
+++ adaptive/__init__.py
@@ -10,7 +10,7 @@ from . import utils
 
 from .learner import (Learner1D, Learner2D, LearnerND, AverageLearner,
                       BalancingLearner, make_datasaver, DataSaver,
-                      IntegratorLearner, AverageLearner2D)
+                      IntegratorLearner, AverageLearner1D, AverageLearner2D)
 
 with suppress(ImportError):
     # Only available if 'scikit-optimize' is installed
