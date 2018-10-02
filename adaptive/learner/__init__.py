0ffec68e016ac1aff54af9b62fb4d300d89a02bc
Bas Nijholt <basnijholt@gmail.com>
2018-10-02 12:01:11 +0200

    create 'AverageMixin'

--- adaptive/learner/__init__.py
+++ adaptive/learner/__init__.py
@@ -3,12 +3,12 @@ from contextlib import suppress
 
 from .average_learner import AverageLearner
 from .average1D import AverageLearner1D
+from .average2D import AverageLearner2D
 from .base_learner import BaseLearner
 from .balancing_learner import BalancingLearner
 from .learner1D import Learner1D
 from .learner2D import Learner2D
 from .learnerND import LearnerND
-from .average2D import AverageLearner2D
 from .integrator_learner import IntegratorLearner
 from .data_saver import DataSaver, make_datasaver
 
